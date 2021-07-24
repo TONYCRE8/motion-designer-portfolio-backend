1fa2c987519f2cc17d66efe7f8ecb7d58cc48c27
Tony <ingallbudder@gmail.com>
2021-07-24 16:02:54 +0100

    update database config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 2412),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4378a81a8cd282e5625128a0265ea925'),
